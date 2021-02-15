b6127cbe6438a689a3aac37521941bbfca481ea9
Slava <1978181@bk.ru>
2021-02-15 20:21:51 +0300

    first commit

---
+++ client/build/precache-manifest.4c395966707880d8a4f7ac6a06f87e84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe69f62cb1c5a1a19677871ca1a8f34",
+    "url": "/KH93b/react-chat.git/index.html"
+  },
+  {
+    "revision": "148affc89f7ab605a6d2",
+    "url": "/KH93b/react-chat.git/static/css/main.910aa548.chunk.css"
+  },
+  {
+    "revision": "6ad28dfb449014558716",
+    "url": "/KH93b/react-chat.git/static/js/2.93c2d232.chunk.js"
+  },
+  {
+    "revision": "9b9de423b52b798b8fc649e7d9e276fb",
+    "url": "/KH93b/react-chat.git/static/js/2.93c2d232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "148affc89f7ab605a6d2",
+    "url": "/KH93b/react-chat.git/static/js/main.fd3e003c.chunk.js"
+  },
+  {
+    "revision": "4c71d82be1b2ffb1916d",
+    "url": "/KH93b/react-chat.git/static/js/runtime-main.18e6390e.js"
+  }
+]);
